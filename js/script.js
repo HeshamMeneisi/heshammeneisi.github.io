4e2df20fa232485cb295db05d43cf217abe6ec88
Hesham Meneisi <heshammeneisi@gmail.com>
2016-09-10 00:53:40 +0200

    Add more projects.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,11 @@ var projects =
 [
 ["Shannon","https://github.com/HeshamMeneisi/Shannon"],
 ["Lone Wolf","https://github.com/HeshamMeneisi/Lone-Wolf"],
-["Inlumino","https://github.com/HeshamMeneisi/Inlumino"]
+["ASMSharp","https://github.com/HeshamMeneisi/ASMSharp"],
+["Inlumino","https://github.com/HeshamMeneisi/Inlumino"],
+["Splash","https://github.com/emam95/Splash"],
+["Sylla",""],
+["WIS",""]
 ];
 
 function* getProjObj(){	
@@ -11,4 +15,26 @@ function* getProjObj(){
 		p = projects[i][0]		
 		yield {name : p,img:"images/"+ p.toLowerCase().replace(" ","")+".png",link:projects[i][1]}
 	}
+}
+
+function writeObj(obj){
+	if(i%3 == 0)
+  	{
+    	document.write("<tr>")
+  	}
+  	i++
+  	var hr = ""
+  	if(obj["link"] != "")
+  		hr = "href=\""+obj["link"]+"\""  	
+	document.write("<td class=\"cell\">"+
+	"<div class=\"hovereffect\">"+
+	    "<img src=\""+obj["img"]+"\" class=\"rimg\">"+
+	    "<div class=\"overlay\">"+
+	       "<h2>"+obj["name"]+"</h2>"+
+	       "<a class=\"info\""+hr+">Go to repo</a>"+
+	    "</div></div></td>")
+	if(i%3 == 0)
+	{
+	  document.write("</tr>")
+	} 
 }
