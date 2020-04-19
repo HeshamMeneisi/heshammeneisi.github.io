e2dbdf61aff6f688051f57ff5e41c941c5c91d34
Hesham <heshammeneisi@gmail.com>
2020-04-20 02:05:58 +0700

    feat(app): add github button overlay

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
-import { Container } from 'semantic-ui-react';
+import { Button, Container, Grid, Icon } from 'semantic-ui-react';
 import HomePage from './HomePage';
 
 function App() {
   return (
     <Container>
       <HomePage />
+      <Grid style={{ position: 'fixed', margin: '1em', bottom: '0px', left: '0px',
+        animation: 'hover 3s ease-in-out infinite', zIndex: 6 }}
+      >
+        <Button
+          color="grey"
+          size="tiny"
+          onClick={() => window.open('https://github.com/heshammeneisi')}
+        >
+          <Icon name="github" />
+          Github
+        </Button>
+      </Grid>
     </Container>
   );
 }
