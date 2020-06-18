1858e4b3cb5c24f73fe162febce0dec1b8cce048
Hesham <heshammeneisi@gmail.com>
2020-06-19 02:44:35 +0700

    feat(resume): add live resume

--- src/components/Floaters.tsx
+++ src/components/Floaters.tsx
@@ -1,8 +1,10 @@
-import { Button, Grid, Icon, Popup } from 'semantic-ui-react';
-import React from 'react';
+import { Button, Grid, Icon, Modal, Popup } from 'semantic-ui-react';
+import React, { useState } from 'react';
 import SvgIcon from './SvgIcon';
 
 function Floaters() {
+  const [dialog, setDialog] = useState(false);
+
   return (
     <Grid style={{ position: 'fixed', margin: '1em', bottom: '0px', left: '0px', zIndex: 6 }}>
       <Popup
@@ -55,6 +57,40 @@ function Floaters() {
           </Button>
         )}
       />
+      <Popup
+        content='Resume'
+        size="mini"
+        inverted
+        position="top center"
+        trigger={(
+          <Button
+            style={{ animation: 'hover 3s ease-in-out infinite', animationDelay: '1s' }}
+            color="red"
+            icon
+            size="tiny"
+            onClick={() => setDialog(true)}
+          >
+            <Icon name="google drive" />
+          </Button>
+        )}
+      />
+      <Modal
+        dimmer="blurring"
+        open={dialog}
+        onClose={() => setDialog(false)}
+        style={{ height: '80%', animation: 'scaleIn 1s' }}
+      >
+        <Modal.Header>My Resume</Modal.Header>
+        <iframe
+          title="Resume"
+          width="100%"
+          height="100%"
+          frameBorder={0}
+          allowFullScreen
+          scrolling="test"
+          src="https://docs.google.com/document/d/e/2PACX-1vTCcFhTHHdNQEeDNrJn7odf4kK2CoXXaS1OOs9gi_0KDfcQn3ZS_CjmdGI9bmMrxX5iZpAb2jePhf-a/pub?embedded=true"
+        />
+      </Modal>
     </Grid>
   );
 }
