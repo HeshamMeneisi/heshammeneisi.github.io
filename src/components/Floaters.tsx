8762cbbf6be79a442eea683b2887b5da6d53000b
Hesham <heshammeneisi@gmail.com>
2020-06-20 19:32:42 +0700

    feat(floaters): make floating buttons more compact

--- src/components/Floaters.tsx
+++ src/components/Floaters.tsx
@@ -7,73 +7,77 @@ function Floaters() {
 
   return (
     <Grid style={{ position: 'fixed', margin: '1em', bottom: '0px', left: '0px', zIndex: 6 }}>
-      <Popup
-        content='Github'
-        size="mini"
-        inverted
-        position="top center"
-        trigger={(
-          <Button
-            style={{ animation: 'hover 3s ease-in-out infinite' }}
-            icon
-            size="tiny"
-            onClick={() => window.open('https://github.com/heshammeneisi')}
-          >
-            <Icon name="github" color="black" />
-          </Button>
+      <Grid.Row style={{ paddingBottom: '2px' }}>
+        <Popup
+          content='Github'
+          size="mini"
+          inverted
+          position="top center"
+          trigger={(
+            <Button
+              style={{ animation: 'grow 3s ease-in-out infinite' }}
+              icon
+              size="tiny"
+              onClick={() => window.open('https://github.com/heshammeneisi')}
+            >
+              <Icon name="github" color="black" />
+            </Button>
+          )}
+        />
+        <Popup
+          content='Upwork'
+          size="mini"
+          inverted
+          position="top center"
+          trigger={(
+            <Button
+              style={{ animation: 'grow 3s ease-in-out infinite', animationDelay: '1s' }}
+              color="green"
+              icon
+              size="tiny"
+              onClick={() => window.open('https://www.upwork.com/freelancers/~01c73274b9b99e8be7')}
+            >
+              <SvgIcon name="upwork" light />
+            </Button>
+          )}
+        />
+      </Grid.Row>
+      <Grid.Row style={{ paddingTop: '2px' }}>
+        <Popup
+          content='LinkedIn'
+          size="mini"
+          inverted
+          position="top center"
+          trigger={(
+            <Button
+              style={{ animation: 'grow 3s ease-in-out infinite', animationDelay: '0.5s' }}
+              color="blue"
+              icon
+              size="tiny"
+              onClick={() => window.open('https://www.linkedin.com/in/hesham-meneisi')}
+            >
+              <SvgIcon name="linkedin" light />
+            </Button>
         )}
-      />
-      <Popup
-        content='Upwork'
-        size="mini"
-        inverted
-        position="top center"
-        trigger={(
-          <Button
-            style={{ animation: 'hover 3s ease-in-out infinite', animationDelay: '1s' }}
-            color="green"
-            icon
-            size="tiny"
-            onClick={() => window.open('https://www.upwork.com/freelancers/~01c73274b9b99e8be7')}
-          >
-            <SvgIcon name="upwork" light />
-          </Button>
+        />
+        <Popup
+          content='Resume'
+          size="mini"
+          inverted
+          position="top center"
+          trigger={(
+            <Button
+              style={{ animation: 'grow 3s ease-in-out infinite', animationDelay: '1.5s' }}
+              color="red"
+              icon
+              size="tiny"
+              onClick={() => setDialog(true)}
+            >
+              <Icon name="google drive" />
+            </Button>
         )}
-      />
-      <Popup
-        content='LinkedIn'
-        size="mini"
-        inverted
-        position="top center"
-        trigger={(
-          <Button
-            style={{ animation: 'hover 3s ease-in-out infinite', animationDelay: '0.5s' }}
-            color="blue"
-            icon
-            size="tiny"
-            onClick={() => window.open('https://www.linkedin.com/in/hesham-meneisi')}
-          >
-            <SvgIcon name="linkedin" light />
-          </Button>
-        )}
-      />
-      <Popup
-        content='Resume'
-        size="mini"
-        inverted
-        position="top center"
-        trigger={(
-          <Button
-            style={{ animation: 'hover 3s ease-in-out infinite', animationDelay: '1s' }}
-            color="red"
-            icon
-            size="tiny"
-            onClick={() => setDialog(true)}
-          >
-            <Icon name="google drive" />
-          </Button>
-        )}
-      />
+        />
+      </Grid.Row>
       <Modal
         dimmer="blurring"
         open={dialog}
